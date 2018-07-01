74b4bbab39a0d6deac6945d150423f87e90f8e37
谷口純也 <taniguchi@taniguchijunya-no-iMac.local>
2018-07-02 02:50:27 +0900

    make it better

--- app/models/programming.rb
+++ app/models/programming.rb
@@ -1,9 +1,7 @@
 class Programming < ApplicationRecord
     validates :title, presence: true
     validates :question, presence: true
-    validates :answer, presence: true
-    validates :url, presence: true
-
+    
         def self.search(search) #self.でクラスメソッドとしている
           if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
             Programming.where(['title LIKE ?', "%#{search}%"])
